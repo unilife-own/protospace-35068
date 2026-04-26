37d91adb895ae9b8700caf8a50bbb3598ee2722a
rana20210222 <raranana95@yahoo.co.jp>
2026-04-26 21:11:47 +0900

    URL変更？

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :create]
 
 
   def index
@@ -53,7 +53,7 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && current_user.id == @prototype.user_id
+    unless user_signed_in? && current_user.id == @prototype.user_id = Prototype.find(params[:id])
       redirect_to action: :index
     end
   end
